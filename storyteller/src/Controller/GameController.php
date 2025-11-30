c12b308a6b849686bece389e18fff836f3d55fc8
Owen Bonsergent <owenbons@gmail.com>
2025-11-30 13:30:53 +0100

    Added ID generation+cookie, username, route to create, route to join

---
+++ storyteller/src/Controller/GameController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class GameController extends AbstractController
+{
+    #[Route('/create-game', name: 'create_game')]
+    public function createGame(): Response
+    {
+        return $this->render('default/create_game.html.twig');
+    }
+
+    #[Route('/join-game', name: 'join_game')]
+    public function joinGame(): Response
+    {
+        return $this->render('default/join_game.html.twig');
+    }
+}
