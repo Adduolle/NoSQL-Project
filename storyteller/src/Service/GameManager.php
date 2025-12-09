3dba4bbcb113a4475a4885b8135df9201915a01b
Klaudia65 <klaudia.kubale@etudiant.univ-lr.fr>
2025-12-09 17:53:19 +0100

    probleme bundle resolu + redirection du home controleur vers index.html.twig
    ajout de bundle de securite
    modif du php gameManager ( code python enlever)

--- storyteller/src/Service/GameManager.php
+++ storyteller/src/Service/GameManager.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Service;
 
 use Psr\Cache\CacheItemPoolInterface;
@@ -8,25 +7,44 @@ use Symfony\Component\HttpFoundation\Request;
 
 class GameManager
 {
-    public function getNextRoundId(string $gameId, int $RoundNumber,string $playerId): string
+    public function getNextRoundId(string $gameId, int $roundNumber, string $playerId): string
     {
-        //todo use redis
+        // TODO: use redis
+        return '';
     }
 
     public function createGame(string $gameId, array $playerIds): void
     {
-        //todo create party with list of player in redis
-
-        //todo use neo4j
-        createGameWithParticipants($gameId, $playerIds);
-        for k in range(len(playerIds)):
-            //createIdHistoire
-            createStory($gameId , $storyId)
-            for j in range(len(playerIds)):
-                createScript($storyId,$scriptId, j)
-
-        //todo create the list of script in order 
+        // TODO: create party with list of player in redis
+        // TODO: use neo4j
+        $this->createGameWithParticipants($gameId, $playerIds);
+        
+        for ($k = 0; $k < count($playerIds); $k++) {
+            // createIdHistoire
+            $storyId = ''; // TODO: générer un ID unique pour l'histoire
+            $this->createStory($gameId, $storyId);
+            
+            for ($j = 0; $j < count($playerIds); $j++) {
+                $scriptId = ''; // TODO: générer un ID unique pour le script
+                $this->createScript($storyId, $scriptId, $j);
+            }
+        }
+        
+        // TODO: create the list of script in order
     }
 
+    private function createGameWithParticipants(string $gameId, array $playerIds): void
+    {
+        // TODO: implémenter la création du jeu avec les participants
+    }
 
+    private function createStory(string $gameId, string $storyId): void
+    {
+        // TODO: implémenter la création de l'histoire
+    }
+
+    private function createScript(string $storyId, string $scriptId, int $index): void
+    {
+        // TODO: implémenter la création du script
+    }
 }
