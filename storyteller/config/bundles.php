32a59d3ce78b341cc3402742fca3212c8cf388ee
Klaudia65 <klaudia.kubale@etudiant.univ-lr.fr>
2025-12-10 21:11:56 +0100

    new environment for twig

--- storyteller/config/bundles.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
-    Neo4j\Neo4jBundle\Neo4jBundle::class => ['all' => true],
-    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-];
